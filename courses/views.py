a18e7a93bd0c007f92fcf6bc4c1427990d62e501
tomkelly111 <>
2023-10-12 14:30:56 +0000

    wire up course_contents.html urls

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def course_contents(request):
+    """a view to return courses page"""
+    return render(request, 'courses/course_contents.html')
