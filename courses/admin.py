d7f5edb1b9c4a1407c155162817303d85f8a4e3d
tomkelly111 <>
2023-10-16 13:52:23 +0000

    add view to display course content and add html for same

--- courses/admin.py
+++ courses/admin.py
@@ -5,9 +5,16 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Course)
 class CourseAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'description', 'price')
     summernote_fields = ('content',)
 
 
 @admin.register(Coach)
 class CoachAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content',)
+    list_display = ('name', 'display_courses')
+    summernote_fields = ('biography', 'accolades')
+
+    def display_courses(self, obj):
+        return ", ".join([course.name for course in obj.courses.all()])
+
+    display_courses.short_description = 'Coaches the following courses:'
--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
 
 
 def course_contents(request):
     """a view to return courses page"""
-    return render(request, 'courses/course_contents.html')
+    courses = Course.objects.all()
+
+    context = {
+        'courses': courses
+    }
+    return render(request, 'courses/course_contents.html', context)
