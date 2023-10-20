2ffd4631217f8fe4e9b02d4421a49ea3ea52004b
tomkelly111 <>
2023-10-20 09:02:15 +0000

    update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from courses.models import Course
 
 
 def bag_contents(request):
@@ -7,6 +9,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for course_id, quantity in bag.items():
+        course = get_object_or_404(Course, pk=course_id)
+        total += course.price
+        product_count += quantity
+        bag_items.append({
+            'course_id': course_id,
+            'quantity': quantity,
+            'course': course,
+        })
 
     context = {
         'bag_items': bag_items,
