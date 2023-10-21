c7ea592b21d312e89aa297066637efcfbc87792e
tomkelly111 <>
2023-10-21 09:07:56 +0000

    added forms, admin and signal

--- checkout/models.py
+++ checkout/models.py
@@ -54,7 +54,6 @@ class OrderLineItem(models.Model):
                               on_delete=models.CASCADE, related_name='lineitems')
     course = models.ForeignKey(
         Course, null=False, blank=False, on_delete=models.CASCADE)
-    quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
