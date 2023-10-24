8c28b1e9baa66235598588dbf5bcb6c3f11ccb56
tomkelly111 <>
2023-10-24 16:02:05 +0000

    added checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.conf import settings
 from django.contrib import messages
 
@@ -8,7 +9,24 @@ from courses.models import Course
 from .models import Order, OrderLineItem
 
 import stripe
+import json
 
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -56,7 +74,7 @@ def checkout(request):
         if not bag:
              messages.error(request, "There's nothing in your cart at the moment")
              return redirect(reverse('course_contents'))
-             
+
         current_bag = bag_contents(request)
         total = current_bag['total']
         stripe_total = round(total * 100)
