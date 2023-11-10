4af82bd3813c8686294dfcb579eebd59aec96d01
tomkelly111 <>
2023-11-10 11:15:42 +0000

    change inviron to enviorn in settings.py

--- snapcallcoaching/settings.py
+++ snapcallcoaching/settings.py
@@ -193,8 +193,8 @@ if 'USE_ASW' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'snapcallcoaching'
     AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACCESS_KEY_ID = os.inviron.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.inviron.get('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # static and media files
