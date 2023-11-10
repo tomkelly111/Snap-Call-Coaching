d8387118ba915444842f371b3ea8659857c61256
tomkelly111 <>
2023-11-10 10:59:14 +0000

    remove cloudinary storage settings

--- snapcallcoaching/settings.py
+++ snapcallcoaching/settings.py
@@ -183,13 +183,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 if 'USE_ASW' in os.environ:
     # Bucket Config
@@ -205,7 +203,7 @@ if 'USE_ASW' in os.environ:
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
-    #override static and media urls in production
+    # override static and media urls in production
     STATIC_URL = f'https://{AWS_S3_CUTSOM_DOMAIN}/{STATICFILES_LOCATION}'
     MEDIA_URL = f'https://{AWS_S3_CUTSOM_DOMAIN}/{MEDIAFILES_LOCATION}'
 # Default primary key field type
