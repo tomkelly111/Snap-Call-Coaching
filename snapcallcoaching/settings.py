eb4b0f1ed6553899dc21efd99da8482b9a61c6cd
tomkelly111 <>
2023-11-10 11:11:30 +0000

    fix typo in settings.py - AWS_S3_CUSTOM_DOMAIN

--- snapcallcoaching/settings.py
+++ snapcallcoaching/settings.py
@@ -195,7 +195,7 @@ if 'USE_ASW' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.inviron.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.inviron.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOME_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # static and media files
     STATICFILES_STORAGE = 'custom_storages.storage.StaticStorage'
@@ -204,8 +204,8 @@ if 'USE_ASW' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # override static and media urls in production
-    STATIC_URL = f'https://{AWS_S3_CUTSOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = f'https://{AWS_S3_CUTSOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
