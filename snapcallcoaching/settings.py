de7459eca5eb6606ef7be6ba09007d37016bfe07
tomkelly111 <thomaspkelly111@gmail.com>
2023-11-11 14:15:46 +0000

    change debug = true

--- snapcallcoaching/settings.py
+++ snapcallcoaching/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = [
     '8000-tomkelly111-snapcallcoa-t8y48uho3pd.ws-eu106.gitpod.io', 'snapcallcoaching.herokuapp.com', 'snapcallcoaching-146b3f7fc4be.herokuapp.com']
