00bcb070a4785c9f595c0122ff3b481a408b445a
tomkelly111 <thomaspkelly111@gmail.com>
2023-11-17 14:10:26 +0000

    remove jshint errors

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -94,7 +94,7 @@ form.addEventListener('submit', function (ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
     
 });
 
