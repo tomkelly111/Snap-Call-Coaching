001dda7d9f6bb79f883289863109fb0807b896fd
tomkelly111 <thomaspkelly111@gmail.com>
2023-11-21 20:17:30 +0000

    add edit course automated tests

---
+++ courses/test_views.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Course
+
+
+class TestViews(TestCase):
+    """
+    Checks that a logged in user that accesses the edit url
+    is redirected to the homepage
+    """
+    def test_edit_course_for_loggedin_user(self):
+        course = Course.objects.create(name='test',
+                                       description='test',
+                                       content='test',
+                                       price=100,
+                                       order=1,
+                                       featured_image='placeholder')
+        self.user = User.objects.create_user(username='testuser',
+                                             password='12345')
+        login = self.client.login(username='testuser', password='12345')
+        response = self.client.get(f'/courses/edit/{course.id}')
+        self.assertRedirects(response, '/')
+
+    """
+    Tests if anonymous user is redirected to login
+    when they try to access the edit course page
+    """
+    def test_edit_course_for_anonymous_user(self):
+        course = Course.objects.create(name='test',
+                                       description='test',
+                                       content='test',
+                                       price=100,
+                                       order=1,
+                                       featured_image='placeholder')
+        response = self.client.get(f'/courses/edit/{course.id}')
+        self.assertRedirects(response,
+                             f'/accounts/login/?next=/'
+                             f'courses/edit/{course.id}')
