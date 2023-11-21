86e45fbc9d184537ec4620a33a22090f217fb508
tomkelly111 <thomaspkelly111@gmail.com>
2023-11-21 18:40:53 +0000

    add automated testing for profile form

---
+++ profiles/test_forms.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from .forms import UserProfileForm
+
+
+class TestUserProfileForm(TestCase):
+
+    def test_fields_not_required(self):
+        form = UserProfileForm({'default_full_name': '',
+                                'default_phone_number': '',
+                                'default_street_address1': '',
+                                'default_street_address2': '',
+                                'default_town_or_city': '',
+                                'default_postcode': '',
+                                'default_country': '',
+                                'default_county': ''})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form(self):
+        form = UserProfileForm()
+        self.assertEqual(form.Meta.exclude,
+                         ('user',
+                          'purchased_courses',
+                          'reviewed_courses'))
