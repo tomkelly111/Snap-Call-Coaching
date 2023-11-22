5217fb6be5cafd87bb6311887b7190306e5d8587
tomkelly111 <thomaspkelly111@gmail.com>
2023-11-22 11:43:46 +0000

    add tests for delete course views

--- courses/test_views.py
+++ courses/test_views.py
@@ -36,3 +36,35 @@ class TestViews(TestCase):
         self.assertRedirects(response,
                              f'/accounts/login/?next=/'
                              f'courses/edit/{course.id}')
+    """
+    Checks that a logged in user that accesses the delete url
+    is redirected to the homepage
+    """
+    def test_delete_course_for_loggedin_user(self):
+        course = Course.objects.create(name='test',
+                                       description='test',
+                                       content='test',
+                                       price=100,
+                                       order=1,
+                                       featured_image='placeholder')
+        self.user = User.objects.create_user(username='testuser',
+                                             password='12345')
+        login = self.client.login(username='testuser', password='12345')
+        response = self.client.get(f'/courses/delete/{course.id}')
+        self.assertRedirects(response, '/')
+
+    """
+    Tests if anonymous user is redirected to login
+    when they try to access the delete course page
+    """
+    def test_delete_course_for_anonymous_user(self):
+        course = Course.objects.create(name='test',
+                                       description='test',
+                                       content='test',
+                                       price=100,
+                                       order=1,
+                                       featured_image='placeholder')
+        response = self.client.get(f'/courses/delete/{course.id}')
+        self.assertRedirects(response,
+                             f'/accounts/login/?next=/'
+                             f'courses/delete/{course.id}')
